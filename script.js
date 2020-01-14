7afe5fa12bbfdde41c210b20c490bf0f386fa9bb
Andy Trevorah <andy@trevorah.net>
2020-01-14 11:24:19 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function hello1() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello2() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello3() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello4() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello5() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello6() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello7() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function hello() {
+console.log( "formatting me is totally out of scope for this PR")
+  }
+
+function add(a,b) {
+  return a-b;
+}
+
+function superUglyCode () {
+const isThisBeingEditedInAnotherBranch = "probably"
+   const shouldItBeFormatted = "not yet";
+}
